5b7abf5bb7a4a63d834425f6e98bfd6fc0755611
gazisu <info.gazisu@gmail.com>
2023-03-12 00:02:45 +0600

    Java Basic Practice

---
+++ src/main/java/Assignment1.java
@@ -0,0 +1,11 @@
+public class Assignment1{
+        public static void main(String[] args) {
+            System.out.println("id: 101");
+            System.out.println("title: iPhone");
+            System.out.println("Type: Phone");
+            System.out.println("Description: iPhone 14 max pro");
+            System.out.println("Price: 151000");
+
+        }
+}
+
---
+++ src/main/java/com/mycompany/basicjava/BioData.java
@@ -0,0 +1,10 @@
+
+package com.mycompany.basicjava;
+public class BioData {
+       public static void main(String[] args){
+   System.out.println("Gazi");
+   System.out.println("Address: Dhanmondi, Dhaka");
+   System.out.println("Mobile No: 0171111111");
+   } 
+    
+}
