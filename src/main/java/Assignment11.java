cf92b271cdd3ffea766e7043b4615712b817a7a4
gazisu <info.gazisu@gmail.com>
2023-03-12 21:24:15 +0600

    Assignment11 added

---
+++ src/main/java/Assignment11.java
@@ -0,0 +1,10 @@
+public class Assignment11 {
+    public static void main(String[] args){
+        System.out.println("Project ID: QABatch 1");
+        System.out.println("Project Name: Testing Automation");
+        System.out.println("Institute: BITM");
+        System.out.println("Location: Basis, Kawran Bazar");
+        System.out.println("Instructor: Rased Karim");
+
+    }
+}
