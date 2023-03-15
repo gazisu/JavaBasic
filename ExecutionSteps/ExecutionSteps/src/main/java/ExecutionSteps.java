c7a4563ef760ca4c2533cd7f7e1e1943aca84d23
gazisu <info.gazisu@gmail.com>
2023-03-15 23:08:33 +0600

    ExecutionSteps added

---
+++ ExecutionSteps/ExecutionSteps/src/main/java/ExecutionSteps.java
@@ -0,0 +1,12 @@
+/*public class ExecutionSteps {
+    public static void main(String[] args){
+        System.out.println("Hellow World");
+    }
+}
+*/
+
+public class ExecutionSteps {
+    public static void main(String[] args){
+        System.out.println("Hellow World");
+    }
+}
